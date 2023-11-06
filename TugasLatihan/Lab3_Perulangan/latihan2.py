687b7c8e2693c5f312e9c4cef7e0c86d22fa80fa
Pynixz <dhiyaulul@gmail.com>
2023-11-06 10:47:26 +0700

    second commit

--- TugasLatihan/Lab3_Perulangan/latihan2.py
+++ TugasLatihan/Lab3_Perulangan/latihan2.py
@@ -1,13 +1,11 @@
-# Minta pengguna untuk memasukkan tiga bilangan
-bilangan1 = int(input("Masukkan bilangan pertama: "))
-bilangan2 = int(input("Masukkan bilangan kedua: "))
-bilangan3 = int(input("Masukkan bilangan ketiga: "))
+import random
+print("===========================================")
+print("= Bilangan acak yang lebih kecil dari 0,5 =")
+print("===========================================")
 
-# Inisialisasi list untuk menyimpan bilangan-bilangan tersebut
-bilangan = [bilangan1, bilangan2, bilangan3]
-
-# Mengurutkan bilangan dari yang terkecil ke yang terbesar
-bilangan.sort()
-
-# Menampilkan hasil pengurutan
-print("Bilangan-bilangan yang telah diurutkan: ", bilangan)
+jum = int( input("Masukan nilai: "))
+i = 0
+while i in range(jum):
+    i += 1
+    angkarandom = random.uniform(0,0.5)
+    print("Bilangan ke :", i, " : ", angkarandom)
